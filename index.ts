9bd45272848eb1643d337963a42f3ce426a6da51
patrick <patrickw6161@gmail.com>
2019-10-03 19:52:29 +0800

    20191003pratice

---
+++ index.ts
@@ -0,0 +1,12 @@
+let a: string[] = ['a', 'b', 'c', 'd'];
+let b: string = ('bb ff dd tt');
+let c: string = ('0123456789');
+let d: string = ('abcdehghijklmnop');
+
+console.log(d.indexOf('d')); //找文字在第幾個
+console.log(b.split(' ')); //把文字辯陣列
+console.log(c.charAt(5)); //從順序找出文字是哪個
+console.log(a[2]); //陣列
+console.log('#'.repeat(100))//重複文字
+
+//20191003 string pratice
